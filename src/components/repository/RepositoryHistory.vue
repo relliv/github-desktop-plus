8de91796d5b56f3194e5cc42ca32b17fda11f502
Eyüp <epsilonelittr@gmail.com>
2026-03-23 22:35:10 +0300

    style: adjust search bar vertical padding in commit history

--- src/components/repository/RepositoryHistory.vue
+++ src/components/repository/RepositoryHistory.vue
@@ -75,7 +75,7 @@
         </div>
 
         <!-- Search bar -->
-        <div class="shrink-0 px-3 pb-2">
+        <div class="shrink-0 px-3 py-2">
           <div class="relative">
             <Search
               class="absolute left-2.5 top-1/2 -translate-y-1/2 size-3.5 text-muted-foreground pointer-events-none"
